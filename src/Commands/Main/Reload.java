103c53c467eb6e5c13be5f41f97b982ea28159fe
Straiker123 <Straiker123@email.cz>
2020-03-07 13:19:59 +0100

    Update by Straiker123

---
+++ src/Commands/Main/Reload.java
@@ -0,0 +1,87 @@
+package Commands.Main;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.util.StringUtil;
+
+import ServerControl.API;
+import ServerControl.Loader;
+import Utils.setting;
+import me.Straiker123.TheAPI;
+import me.Straiker123.TheRunnable;
+
+public class Reload implements CommandExecutor, TabCompleter {
+	void stop(CommandSender s) {
+		if(r!=null)
+		r.cancel();
+		r=null;
+		Loader.msg("&eSaving worlds and players..", s);
+		for(World w: Bukkit.getWorlds()) {
+			w.save();
+		}
+		Bukkit.savePlayers();
+		TheAPI.broadcastMessage("&cReload of server..");
+		Bukkit.reload();
+	}
+	TheRunnable r;
+	public boolean stop;
+	@Override
+	public boolean onCommand(CommandSender s, Command arg1, String arg2, String[] args) {
+		if(API.hasPerm(s, "ServerControl.Restart")) {
+			int time = Loader.config.getInt("Options.WarningSystem.Reload.PauseTime");
+			if(args.length==1) {
+				if(args[0].equalsIgnoreCase("cancel")) {
+					r.cancel();
+					stop=false;
+					TheAPI.broadcastMessage("&eServer reload has been &acanceled");
+					return true;
+				}else
+				if(args[0].equalsIgnoreCase("now")) {
+					stop(s);
+					return true;
+				}else {
+					time=(int)TheAPI.getStringUtils().getTimeFromString(args[0]);
+				}
+			}
+		if(!stop) {
+			stop=true;
+			r=new TheRunnable();
+			int h = time;
+			if(setting.warn_reload) {
+			r.runRepeating(new Runnable() {
+				int f= h;
+				public void run() {
+					
+					if(f<=0) {
+						stop(s);
+						return;
+					}else if(f == h%75 && h > 15  ||f == h%50 && h > 10 ||f == h%25 && h > 5  || f == 5 || f==4||f==3||f==2||f==1){
+						for(String s:Loader.config.getStringList("Options.WarningSystem.Reload.Messages"))
+							TheAPI.broadcastMessage(s.replace("%time%", ""+TheAPI.getStringUtils().setTimeToString(f)));
+					}
+					--f;
+				}}, 20);
+			}else {
+				stop(s);
+			}
+			return true;
+		}
+		}
+		return true;
+	}
+	@Override
+	public List<String> onTabComplete(CommandSender s, Command arg1, String arg2, String[] args) {
+		List<String> c = new ArrayList<>();
+		if(s.hasPermission("ServerControl.Reload") && args.length==1)
+		c.addAll(StringUtil.copyPartialMatches(args[0], Arrays.asList("15s","30s","now","cancel"), new ArrayList<>()));
+		return c;
+	}
+}
