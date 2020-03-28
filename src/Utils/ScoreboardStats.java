975d7c0a6693718415c6a0328b5cf9f48f508bbe
BuildTools <unconfigured@null.spigotmc.org>
2020-03-28 01:03:42 +0100

    Optimized

--- src/Utils/ScoreboardStats.java
+++ src/Utils/ScoreboardStats.java
@@ -1,7 +1,10 @@
 package Utils;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
 import ServerControl.Loader;
@@ -9,21 +12,31 @@ import me.Straiker123.ScoreboardAPIV2;
 import me.Straiker123.TheAPI;
 
 public class ScoreboardStats {
-
+	public ScoreboardStats() {
+		f=Loader.scFile;
+	}
+	static FileConfiguration f;
 	 static HashMap<String, ScoreboardAPIV2> setup = new HashMap<String, ScoreboardAPIV2>();
 	 public static void createScoreboard(Player p) {
 		 if(!setup.containsKey(p.getName()))setup.put(p.getName(), TheAPI.getScoreboardAPIV2(p));
 		 ScoreboardAPIV2 a=setup.get(p.getName());
-			String getName = "Name";
-			 String getLine = "Lines";
-			 if(setting.sb_world ) {
-				 if( Loader.scFile.getString("PerWorld."+p.getWorld().getName()+".Name")!=null )
+			String getName = null;
+			 List<String> getLine = new ArrayList<String>();
+			 if(setting.sb_world) {
+				 if(f.getString("PerWorld."+p.getWorld().getName()+".Name")!=null)
 					 getName="PerWorld."+p.getWorld().getName()+".Name";
-				 if( Loader.scFile.getString("PerWorld."+p.getWorld().getName()+".Lines")!=null )
-					 getLine="PerWorld."+p.getWorld().getName()+".Lines";
+				 else
+					 getName=f.getString("Name");
+				 if(f.getString("PerWorld."+p.getWorld().getName()+".Lines")!=null)
+					 getLine=f.getStringList("PerWorld."+p.getWorld().getName()+".Lines");
+				 else
+					 getName=f.getString("Lines");
+			 }else {
+				 getName=f.getString("Name");
+				 getLine=f.getStringList("Lines");
 			 }
-			a.setTitle(Loader.scFile.getString(getName));
-			for(String ss: Loader.scFile.getStringList(getLine)) {
+			a.setTitle(getName);
+			for(String ss:getLine) {
 				a.addLine(TheAPI.getPlaceholderAPI().setPlaceholders(p,TabList.replace(ss, p)));
 			}
 			 a.create();
