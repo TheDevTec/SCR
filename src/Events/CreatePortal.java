053051281bfc2c1d30e9920f4e8db0acde56b4f2
BuildTools <unconfigured@null.spigotmc.org>
2020-04-22 19:38:51 +0200

    Fixed portal create/teleport

--- src/Events/CreatePortal.java
+++ src/Events/CreatePortal.java
@@ -1,20 +1,31 @@
 package Events;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPhysicsEvent;
 import org.bukkit.event.entity.EntityPortalEvent;
-import org.bukkit.event.world.PortalCreateEvent;
+import org.bukkit.event.player.PlayerPortalEvent;
 
 import ServerControl.Loader;
 
 public class CreatePortal implements Listener {
-	@EventHandler(priority = EventPriority.LOWEST)
-	public void OnCreatePortal(PortalCreateEvent e) {
-		if(Loader.mw.getBoolean("WorldsSettings."+e.getWorld().getName()+".CreatePortal")==false)e.setCancelled(true);
+	@EventHandler(priority=EventPriority.LOWEST)
+	public void onEndPortal(BlockPhysicsEvent e) {
+		Bukkit.broadcastMessage(e.getChangedType().name());
+		if(e.getBlock().getType().name().equals("END_PORTAL")||e.getBlock().getType().name().equals("NETHER_PORTAL"))
+		if(!Loader.mw.getBoolean("WorldsSettings."+e.getBlock().getWorld().getName()+".CreatePortal"))e.getBlock().setType(Material.AIR);
 	}
+
 	@EventHandler(priority = EventPriority.LOWEST)
-	public void OnEntityPortalTravel(EntityPortalEvent e) {
-		if(Loader.mw.getBoolean("WorldsSettings."+e.getFrom().getWorld().getName()+".PortalTeleport")==false)e.setCancelled(true);
+	public void onPortalTravel(PlayerPortalEvent e) { //not working for player
+		if(!Loader.mw.getBoolean("WorldsSettings."+e.getFrom().getWorld().getName()+".PortalTeleport"))e.setCancelled(true);
+	}
+	
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onPortalTravel(EntityPortalEvent e) { //not working for player
+		if(!Loader.mw.getBoolean("WorldsSettings."+e.getFrom().getWorld().getName()+".PortalTeleport"))e.setCancelled(true);
 	}
 }
