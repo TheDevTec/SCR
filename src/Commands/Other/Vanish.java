fc734bf8320113134b25ea910d3f07e3ac42f74d
Rajskoo <yoda.reismuller@gmail.com>
2020-10-15 20:10:43 +0200

    Added action bar info, when player is in vanish mode

--- src/Commands/Other/Vanish.java
+++ src/Commands/Other/Vanish.java
@@ -8,8 +8,11 @@ import org.bukkit.entity.Player;
 import ServerControl.Loader;
 import ServerControl.Loader.Placeholder;
 import me.DevTec.TheAPI.TheAPI;
+import me.DevTec.TheAPI.Scheduler.Scheduler;
+import me.DevTec.TheAPI.Scheduler.Tasker;
 
 public class Vanish implements CommandExecutor {
+	public static int i = -1;
 
 	@Override
 	public boolean onCommand(CommandSender s, Command cmd, String label, String[] args) {
@@ -20,10 +23,19 @@ public class Vanish implements CommandExecutor {
 					if (!TheAPI.isVanished(p)) {
 						TheAPI.vanish(p, "ServerControl.Vanish", true);
 						Loader.sendMessages(s, "Vanish.Enabled.You");
+						Scheduler.cancelTask(i);
 						return true;
 					}
 					TheAPI.vanish(p, "ServerControl.Vanish", false);
 					Loader.sendMessages(s, "Vanish.Disabled.You");
+					if (i == -1) {
+						i = new Tasker( ) {
+							@Override
+							public void run() {
+								TheAPI.sendActionBar(p, Loader.getTranslation("Vanish.Active").toString());
+							}
+						}.runRepeatingSync(0, 20);
+					}
 					return true;
 				}
 				Loader.Help(s, "Vanish", "Other");
@@ -36,11 +48,20 @@ public class Vanish implements CommandExecutor {
 					TheAPI.vanish(t, "ServerControl.Vanish", true);
 					Loader.sendMessages(s, "Vanish.Enabled.Other.Sender", Placeholder.c().add("%player%", t.getName()).add("%playername%", t.getDisplayName()));
 					Loader.sendMessages(s, "Vanish.Enabled.Other.Receiver", Placeholder.c().add("%player%", s.getName()).add("%playername%", s.getName()));
+					if (i == -1) {
+						i = new Tasker( ) {
+							@Override
+							public void run() {
+								TheAPI.sendActionBar(t, Loader.getTranslation("Vanish.Active").toString());
+							}
+						}.runRepeatingSync(0, 20);
+					}
 					return true;
 				}
 				TheAPI.vanish(t, "ServerControl.Vanish", false);
 				Loader.sendMessages(s, "Vanish.Disabled.Other.Sender", Placeholder.c().add("%player%", t.getName()).add("%playername%", t.getDisplayName()));
 				Loader.sendMessages(s, "Vanish.Disabled.Other.Receiver", Placeholder.c().add("%player%", s.getName()).add("%playername%", s.getName()));
+				Scheduler.cancelTask(i);
 				return true;
 			}
 			Loader.notOnline(s, args[0]);
