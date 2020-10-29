c6965798af905810c883fc93f7fed2c173c1c0f9
petulikan1 <petulikan@gmail.com>
2020-10-29 18:46:04 +0100

    Repair

--- src/Commands/Other/Item.java
+++ src/Commands/Other/Item.java
@@ -1,25 +1,14 @@
 package Commands.Other;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabCompleter;
 
-public class Item implements CommandExecutor, TabCompleter {
+public class Item implements CommandExecutor{
 
 	@Override
 	public boolean onCommand(CommandSender s, Command arg1, String arg2, String[] args) {
-		//must rewrite code
 		return true;
 	}
 	
-	@Override
-	public List<String> onTabComplete(CommandSender s, Command cmd, String arg2, String[] args) {
-		List<String> c = new ArrayList<>();
-		//must rewrite code
-		return c;
-	}
 }
