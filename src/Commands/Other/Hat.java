cf805ddbb0f267e8501ff80dcd411f9d2582c727
Rajskoo <yoda.reismuller@gmail.com>
2020-10-31 19:54:57 +0100

    Started editing Hat.class

--- src/Commands/Other/Hat.java
+++ src/Commands/Other/Hat.java
@@ -5,6 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import ServerControl.Loader;
@@ -23,28 +24,57 @@ public class Hat implements CommandExecutor {
 					Loader.sendMessages(s, "Missing.HandEmpty");
 					return true;
 				}
+				Inventory inv = p.getInventory();
 				if (args.length == 0) {
-					if (p.getInventory().getHelmet() != null)
-						p.getInventory().addItem(p.getInventory().getHelmet());
-					p.getInventory().setHelmet(p.getEquipment().getItemInMainHand());
-					p.getInventory().setItemInHand(new ItemStack(Material.AIR));
-					Loader.sendMessages(s, "Hat.Equipped.You", Placeholder.c().replace("%item%", p.getInventory().getHelmet().getType().name()));
-					return true;
+					boolean check = false;
+					for (ItemStack item : inv.getStorageContents()) {
+						if (item == null) {
+							check = true;
+							break;
+						}
+					}
+					if (!check) {
+						Loader.sendMessages(p, "Hat.InvFull.You");
+						return true;
+					} else if (check == true) {
+						if (p.getInventory().getHelmet() != null)
+							p.getInventory().addItem(p.getInventory().getHelmet());
+						p.getInventory().setHelmet(p.getEquipment().getItemInMainHand());
+						p.getInventory().setItemInHand(new ItemStack(Material.AIR));
+						Loader.sendMessages(s, "Hat.Equipped.You", Placeholder.c().replace("%item%", p.getInventory().getHelmet().getType().name()));
+						return true;
+					}
 				}
 				if (Loader.has(s, "Hat", "Other", "Other")) {
+					if (args.length == 1) {
 				Player t = TheAPI.getPlayer(args[0]);
 				if (t == null) {
 					Loader.notOnline(s,args[0]);
 					return true;
 				}
-				if (t.getInventory().getHelmet() != null)
-					t.getInventory().addItem(t.getInventory().getHelmet());
-				t.getInventory().setHelmet(p.getEquipment().getItemInMainHand());
-				p.getInventory().setItemInHand(new ItemStack(Material.AIR));
-				Loader.sendMessages(s, "Hat.Equipped.Other.Sender", Placeholder.c().replace("%player%", t.getName()).replace("%playername%", t.getDisplayName()).replace("%item%", t.getInventory().getHelmet().getType().name()));
-				Loader.sendMessages(t, "Hat.Equipped.Other.Receiver", Placeholder.c().replace("%player%", s.getName()).replace("%playername%", s.getName()).replace("%item%", t.getInventory().getHelmet().getType().name()));
-				return true;
+				inv = t.getInventory();
+				boolean check = false;
+				for (ItemStack item : inv.getStorageContents()) {
+					if (item == null) {
+						check = true;
+						break;
+					}
 				}
+				if(!check) {
+					Loader.sendMessages(t, "Hat.Invfull.Other", Placeholder.c()
+							.add("%player%", t.getName())
+							.add("%playername%", t.getDisplayName()));
+				} else if (check) {
+					if (t.getInventory().getHelmet() != null)
+						t.getInventory().addItem(t.getInventory().getHelmet());
+					t.getInventory().setHelmet(p.getEquipment().getItemInMainHand());
+					p.getInventory().setItemInHand(new ItemStack(Material.AIR));
+					Loader.sendMessages(s, "Hat.Equipped.Other.Sender", Placeholder.c().replace("%player%", t.getName()).replace("%playername%", t.getDisplayName()).replace("%item%", t.getInventory().getHelmet().getType().name()));
+					Loader.sendMessages(t, "Hat.Equipped.Other.Receiver", Placeholder.c().replace("%player%", s.getName()).replace("%playername%", s.getName()).replace("%item%", t.getInventory().getHelmet().getType().name()));
+					return true;
+				}
+			}
+		}
 				Loader.noPerms(s, "Hat", "Other", "Other");
 				return true;
 			}
