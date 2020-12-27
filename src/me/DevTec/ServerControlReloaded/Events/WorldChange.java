2829b3affb9a08718184e8eaa00203336c3754f4
BuildTools <unconfigured@null.spigotmc.org>
2020-12-28 00:03:04 +0100

    Fixed bugs, New functions

--- src/me/DevTec/ServerControlReloaded/Events/WorldChange.java
+++ src/me/DevTec/ServerControlReloaded/Events/WorldChange.java
@@ -1,22 +1,78 @@
 package me.DevTec.ServerControlReloaded.Events;
 
+import java.util.Map;
+import java.util.Set;
+
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.event.player.PlayerBedEnterEvent.BedEnterResult;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerGameModeChangeEvent;
 
 import me.DevTec.ServerControlReloaded.SCR.API;
 import me.DevTec.ServerControlReloaded.Utils.SPlayer;
 import me.DevTec.ServerControlReloaded.Utils.setting;
+import me.devtec.theapi.scheduler.Scheduler;
+import me.devtec.theapi.scheduler.Tasker;
+import me.devtec.theapi.utils.datakeeper.collections.UnsortedSet;
+import me.devtec.theapi.utils.datakeeper.maps.UnsortedMap;
+import me.devtec.theapi.utils.reflections.Ref;
 
 public class WorldChange implements Listener {
 	
+	Map<String, Integer> sleepTask = new UnsortedMap<>();
+	Set<Player> sleeping = new UnsortedSet<>();
+	
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onSleep(PlayerBedEnterEvent e) {
+		if (setting.singeplayersleep && e.getBedEnterResult()==BedEnterResult.OK) {
+			World w = e.getBed().getWorld();
+			if(!sleepTask.containsKey(w.getName())) {
+				sleepTask.put(w.getName(), new Tasker() {
+					
+					long start = w.getTime();
+					boolean doNight = !w.isThundering() || start < 2000 && start > 300;
+					public void run() {
+						for(Player s : sleeping) {
+							int old = (int) Ref.get(Ref.player(s), "sleepTicks");
+							if(old >= 98) {
+								Ref.set(Ref.player(s), "sleepTicks", 98);
+							}
+						}
+						if(start+50 > 24000) {
+							start=0;
+							doNight=true;
+						}
+						if(start > 1000) {
+							if(doNight) {
+								cancel();
+								return;
+							}
+						}
+						
+						w.setTime(start);
+						start=start+50;
+					}
+				}.runRepeatingSync(0, 1));
+			}
+			sleeping.add(e.getPlayer());
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onSleep(PlayerBedLeaveEvent e) {
 		if (setting.singeplayersleep) {
-			e.getPlayer().getWorld().setTime(1000);
+			sleeping.remove(e.getPlayer());
+			if(sleeping.isEmpty())
+			if(sleepTask.containsKey(e.getBed().getWorld().getName())) {
+				Scheduler.cancelTask(sleepTask.get(e.getBed().getWorld().getName()));
+				sleepTask.remove(e.getBed().getWorld().getName());
+			}
 		}
 	}
 
@@ -37,11 +93,15 @@ public class WorldChange implements Listener {
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onChangeGamamode(PlayerGameModeChangeEvent e) {
 		SPlayer a = API.getSPlayer(e.getPlayer());
-		if (a.hasFlyEnabled())
-			a.enableFly();
-		if (a.hasTempFlyEnabled())
-			a.enableTempFly();
-		if (a.hasGodEnabled())
-			a.enableGod();
+		new Tasker() {
+			public void run() {
+				if (a.hasFlyEnabled())
+					a.enableFly();
+				if (a.hasTempFlyEnabled())
+					a.enableTempFly();
+				if (a.hasGodEnabled())
+					a.enableGod();
+			}
+		}.runTaskSync();
 	}
 }
