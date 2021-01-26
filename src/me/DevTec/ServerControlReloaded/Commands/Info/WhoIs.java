cf3084b5a73f7722d90d902d20caa8a786967bf8
BuildTools <unconfigured@null.spigotmc.org>
2021-01-26 17:24:54 +0100

    WhoIs command is now async

--- src/me/DevTec/ServerControlReloaded/Commands/Info/WhoIs.java
+++ src/me/DevTec/ServerControlReloaded/Commands/Info/WhoIs.java
@@ -21,6 +21,7 @@ import me.DevTec.ServerControlReloaded.Utils.SPlayer;
 import me.devtec.theapi.TheAPI;
 import me.devtec.theapi.economyapi.EconomyAPI;
 import me.devtec.theapi.punishmentapi.PunishmentAPI;
+import me.devtec.theapi.scheduler.Tasker;
 
 public class WhoIs implements CommandExecutor, TabCompleter {
 
@@ -55,33 +56,37 @@ public class WhoIs implements CommandExecutor, TabCompleter {
 						.add("%player%", a[0]));
 				return true;
 			}
-			String ip = PunishmentAPI.getIP(a[0]);
-			if (ip == null)
-				ip = "Unknown";
-			String country = getCountry(ip);
-			boolean d = false;
-			String afk = "false";
-			String seen = null;
-			if (TheAPI.getPlayerOrNull(a[0]) != null) {
-				seen = API.getSeen(a[0], SeenType.Online);
-				if (API.isAFK(TheAPI.getPlayerOrNull(a[0])))
-					afk = "true";
-				d=true;
-			}else seen = API.getSeen(a[0], SeenType.Offline);
-			SPlayer c = API.getSPlayer(a[0]);
-			Loader.sendMessages(s, "WhoIs."+(d?"Online":"Offline"), Placeholder.c().add("%player%", c.getName()).add("%playername%", c.getName()).add("%customname%", c.getCustomName()).add("%ip%", ip).add("%country%", country)
-					.add("%afk%", afk).add("%seen%", seen).add("%fly%", c.hasFlyEnabled()+"").add("%god%", c.hasGodEnabled()+"").add("%tempfly%", c.hasTempFlyEnabled()+"")
-					.add("%op%", Bukkit.getOperators().contains(Bukkit.getOfflinePlayer(a[0]))+"").add("%uuid%", Bukkit.getOfflinePlayer(a[0]).getUniqueId().toString())
-					.add("%vanish%", c.hasVanish()+"").add("%firstjoin%", TheAPI.getUser(a[0]).getString("FirstJoin")+"").add("%group%", Staff.getGroup(a[0]))
-					.add("%money%", EconomyAPI.format(EconomyAPI.getBalance(a[0])))
-					.add("%health%", c.getPlayer()!=null ? c.getPlayer().getHealthScale()+"":"Unknown").add("%food%", c.getPlayer()!=null ? c.getPlayer().getFoodLevel()+"":"Unknown")
-					.add("%xp%", c.getPlayer()!=null ? c.getPlayer().getTotalExperience()+"":"Unknown").add("%level%", c.getPlayer()!=null ? c.getPlayer().getLevel()+"":"Unknown")
-					.add("%banned%", PunishmentAPI.getBanList(c.getName()).isBanned()+"")
-					.add("%arrested%", PunishmentAPI.getBanList(c.getName()).isJailed()+"")
-					.add("%ip-arrested%", PunishmentAPI.getBanList(c.getName()).isIPJailed()+"")
-					.add("%ip-muted%", PunishmentAPI.getBanList(c.getName()).isIPMuted()+"")
-					.add("%muted%", PunishmentAPI.getBanList(c.getName()).isMuted()+"")
-					.add("%ip-banned%", PunishmentAPI.getBanList(c.getName()).isIPBanned()+""));
+			Loader.sendMessages(s, "WhoIs.Loading", Placeholder.c().add("%player%", a[0]));
+			new Tasker() {
+				public void run() {
+					String ip = PunishmentAPI.getIP(a[0]);
+					if (ip == null)
+						ip = "Unknown";
+					String country = getCountry(ip);
+					boolean d = false;
+					String afk = "false";
+					String seen = null;
+					if (TheAPI.getPlayerOrNull(a[0]) != null) {
+						seen = API.getSeen(a[0], SeenType.Online);
+						if (API.isAFK(TheAPI.getPlayerOrNull(a[0])))
+							afk = "true";
+						d=true;
+					}else seen = API.getSeen(a[0], SeenType.Offline);
+					SPlayer c = API.getSPlayer(a[0]);
+					Loader.sendMessages(s, "WhoIs."+(d?"Online":"Offline"), Placeholder.c().add("%player%", c.getName()).add("%playername%", c.getName()).add("%customname%", c.getCustomName()).add("%ip%", ip).add("%country%", country)
+							.add("%afk%", afk).add("%seen%", seen).add("%fly%", c.hasFlyEnabled()+"").add("%god%", c.hasGodEnabled()+"").add("%tempfly%", c.hasTempFlyEnabled()+"")
+							.add("%op%", Bukkit.getOperators().contains(Bukkit.getOfflinePlayer(a[0]))+"").add("%uuid%", Bukkit.getOfflinePlayer(a[0]).getUniqueId().toString())
+							.add("%vanish%", c.hasVanish()+"").add("%firstjoin%", TheAPI.getUser(a[0]).getString("FirstJoin")+"").add("%group%", Staff.getGroup(a[0]))
+							.add("%money%", EconomyAPI.format(EconomyAPI.getBalance(a[0])))
+							.add("%health%", c.getPlayer()!=null ? c.getPlayer().getHealthScale()+"":"Unknown").add("%food%", c.getPlayer()!=null ? c.getPlayer().getFoodLevel()+"":"Unknown")
+							.add("%xp%", c.getPlayer()!=null ? c.getPlayer().getTotalExperience()+"":"Unknown").add("%level%", c.getPlayer()!=null ? c.getPlayer().getLevel()+"":"Unknown")
+							.add("%banned%", PunishmentAPI.getBanList(c.getName()).isBanned()+"")
+							.add("%arrested%", PunishmentAPI.getBanList(c.getName()).isJailed()+"")
+							.add("%ip-arrested%", PunishmentAPI.getBanList(c.getName()).isIPJailed()+"")
+							.add("%ip-muted%", PunishmentAPI.getBanList(c.getName()).isIPMuted()+"")
+							.add("%muted%", PunishmentAPI.getBanList(c.getName()).isMuted()+"")
+							.add("%ip-banned%", PunishmentAPI.getBanList(c.getName()).isIPBanned()+""));
+				}}.runTask();
 			return true;
 		}
 		Loader.noPerms(s, "WhoIs", "Info");
