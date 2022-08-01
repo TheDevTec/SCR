4a78d24d965ee8cf4970d64c8df78ba738d9e460
Houska02 <ADAMBABOVAK@seznam.cz>
2022-08-01 15:41:11 +0200

    Fixed Thor messages

--- src/me/devtec/scr/commands/fun/Thor.java
+++ src/me/devtec/scr/commands/fun/Thor.java
@@ -9,6 +9,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.util.BlockIterator;
 
+import me.devtec.scr.MessageUtils.Placeholders;
 import me.devtec.scr.commands.ScrCommand;
 import me.devtec.shared.commands.selectors.Selector;
 import me.devtec.shared.commands.structures.CommandStructure;
@@ -16,15 +17,16 @@ import me.devtec.shared.commands.structures.CommandStructure;
 public class Thor implements ScrCommand {
 
 	@Override
-	public void init(List<String> cmds) {
+	public void init(int cd, List<String> cmds) {
 		CommandStructure.create(CommandSender.class, PERMS_CHECKER, (s, structure, args) -> { // cmd
 			if(s instanceof Player) {
 				Player p = (Player)s;
 				Location loc = getLookingBlock(p, 30).getLocation();
 				loc.getWorld().strikeLightningEffect(loc);
-				msgSec(s, "pos", loc.getWorld().getName(), loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
+				msgSec(s, "oc", Placeholders.c().replace("world", loc.getWorld()).replace("x", loc.getBlockX())
+						.replace("y", loc.getBlockY()).replace("z", loc.getBlockZ()) );
 			}else {
-				help(s, 0);
+				help(s, "usage");
 			}
 		}).fallback((s, structure, args) -> {
 			offlinePlayer(s, args[0]);
@@ -35,14 +37,14 @@ public class Thor implements ScrCommand {
 					Location loc = getLookingBlock(p, 30).getLocation();
 					loc.getWorld().strikeLightningEffect(loc);
 				}else {
-					help(s, 0);
+					help(s, "usage");
 				}
 			})
 			.parent() // cmd
 			.selector(Selector.ENTITY_SELECTOR, (s, structure, args) -> { // cmd [entity_selector]
 				for(Player p : playerSelectors(s, args[0])) {
 					p.getWorld().strikeLightning(p.getLocation());
-					msgSec(s, "player", p.getName());
+					msgSec(s, "player", Placeholders.c().replace("target", p.getName()));
 				}
 			}).permission("scr."+configSection()+".player")
 				.argument("-s", (s, structure, args) -> { // cmd [entity_selector] -s
