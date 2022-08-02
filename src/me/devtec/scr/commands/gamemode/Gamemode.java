e7b18b626dee39cad39f3b7594578695ce522541
Houska02 <ADAMBABOVAK@seznam.cz>
2022-08-02 20:51:00 +0200

    Foundation of gamemodes

---
+++ src/me/devtec/scr/commands/gamemode/Gamemode.java
@@ -0,0 +1,20 @@
+package me.devtec.scr.commands.gamemode;
+
+import java.util.List;
+
+import me.devtec.scr.commands.ScrCommand;
+
+public class Gamemode implements ScrCommand{
+
+	@Override
+	public void init(List<String> cmds) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String configSection() {
+		return "gamemode";
+	}
+
+}
