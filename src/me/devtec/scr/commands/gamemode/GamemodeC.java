0e1236e1f6d6220a7ddbafde0fc4c581c23367e9
Houska02 <ADAMBABOVAK@seznam.cz>
2022-08-02 20:57:02 +0200

    Gamemode creative 5%

--- src/me/devtec/scr/commands/gamemode/GamemodeC.java
+++ src/me/devtec/scr/commands/gamemode/GamemodeC.java
@@ -2,13 +2,34 @@ package me.devtec.scr.commands.gamemode;
 
 import java.util.List;
 
+import org.bukkit.GameMode;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
 import me.devtec.scr.commands.ScrCommand;
+import me.devtec.shared.commands.selectors.Selector;
+import me.devtec.shared.commands.structures.CommandStructure;
 
 public class GamemodeC implements ScrCommand{
 
 	@Override
 	public void init(List<String> cmds) {
-		// TODO Auto-generated method stub
+		
+		CommandStructure.create(CommandSender.class, PERMS_CHECKER, (s, structure, args) -> { // cmd
+			if(s instanceof Player) {
+				Player p = (Player)s;
+				
+				p.setGameMode(GameMode.CREATIVE);
+				msg(s, "gamemodes.target."+p.getGameMode().name().toLowerCase());
+			}else {
+				help(s, "usage");
+			}
+		}).cooldownDetection((s, structure, args) -> inCooldown(s))
+		.permission(permission("cmd"))
+		.selector(Selector.PLAYER, (s, structure, args) ->{
+			//TODO
+		})
+		;
 		
 	}
 
