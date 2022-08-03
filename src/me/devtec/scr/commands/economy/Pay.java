067ef2c405174494345d821932bc95195930079e
StraikerinaCZ <Straiker123@email.cz>
2022-08-03 18:53:09 +0200

    Removed unused imports

--- src/me/devtec/scr/commands/economy/Pay.java
+++ src/me/devtec/scr/commands/economy/Pay.java
@@ -13,50 +13,43 @@ import me.devtec.shared.commands.structures.CommandStructure;
 import net.milkbowl.vault.economy.Economy;
 
 public class Pay implements ScrCommand {
-	
-	@SuppressWarnings("deprecation")
+
 	@Override
 	public void init(List<String> cmds) {
-		if(Loader.economy == null)return;
+		if (Loader.economy == null)
+			return;
 		// /pay [player] [amount]
 		CommandStructure.create(Player.class, PLAYER_PERMS_CHECKER, (s, structure, args) -> { // cmd
 			help(s, "usage");
-		}).cooldownDetection((s, structure, args) -> inCooldown(s))
-		.permission(permission("cmd")).fallback((s, structure, args) -> {
+		}).cooldownDetection((s, structure, args) -> inCooldown(s)).permission(permission("cmd")).fallback((s, structure, args) -> {
 			offlinePlayer(s, args[0]);
-			})
-			.selector(Selector.ENTITY_SELECTOR, (s, structure, args) -> { // cmd [entity_selector]
-				help(s, "usage");
-			}).fallback((s, structure, args) -> {
-				help(s, "usage");
-				})
-				.argument(null, (s, structure, args) -> { // cmd [entity_selector] [any string]
-					double money = ScrEconomy.balanceFromString(args[1]);
-					String bal = ((Economy)Loader.economy).format(money);
+		}).selector(Selector.ENTITY_SELECTOR, (s, structure, args) -> { // cmd [entity_selector]
+			help(s, "usage");
+		}).fallback((s, structure, args) -> {
+			help(s, "usage");
+		}).argument(null, (s, structure, args) -> { // cmd [entity_selector] [any string]
+			double money = ScrEconomy.balanceFromString(args[1]);
+			String bal = ((Economy) Loader.economy).format(money);
 
-					Economy ec = (Economy)Loader.economy;
-					if(ec.has(s.getName(), money)) {
-					
-						for(Player p : playerSelectors(s, args[0])) {
-							pay(s, p, money);
-							
-							msgSec(s, "sender", Placeholders.c().addPlayer("target", p).replace("money", bal));
-							msgSec(p, "target", Placeholders.c().addPlayer("player", s).replace("money", bal));
-						}
-					}
-					else
-						msgSec(s, "notEnoughMoney", Placeholders.c().replace("money", bal));
-					})
-					.argument("-s", (s, structure, args) -> { // cmd [entity_selector] [any string] -s
-						double money = ScrEconomy.balanceFromString(args[1]);
-						for(Player p : playerSelectors(s, args[0])) {
-							pay(s, p, money);
-						}
-					}).build().register(cmds.remove(0), cmds.toArray(new String[0]));
+			Economy ec = (Economy) Loader.economy;
+			if (ec.has(s.getName(), money))
+				for (Player p : playerSelectors(s, args[0])) {
+					pay(s, p, money);
+
+					msgSec(s, "sender", Placeholders.c().addPlayer("target", p).replace("money", bal));
+					msgSec(p, "target", Placeholders.c().addPlayer("player", s).replace("money", bal));
+				}
+			else
+				msgSec(s, "notEnoughMoney", Placeholders.c().replace("money", bal));
+		}).argument("-s", (s, structure, args) -> { // cmd [entity_selector] [any string] -s
+			double money = ScrEconomy.balanceFromString(args[1]);
+			for (Player p : playerSelectors(s, args[0]))
+				pay(s, p, money);
+		}).build().register(cmds.remove(0), cmds.toArray(new String[0]));
 	}
-	
+
 	private void pay(Player sender, Player target, double money) {
-		Economy ec = (Economy)Loader.economy;
+		Economy ec = (Economy) Loader.economy;
 		ec.withdrawPlayer(sender, money);
 		ec.depositPlayer(target, money);
 	}
@@ -65,5 +58,5 @@ public class Pay implements ScrCommand {
 	public String configSection() {
 		return "pay";
 	}
-	
+
 }
