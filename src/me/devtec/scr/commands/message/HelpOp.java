e43910ea56d36465d59c621695b2dd43bafcb7b3
Houska02 <Houska02@users.noreply.github.com>
2022-08-05 12:53:17 +0200

    git test

--- src/me/devtec/scr/commands/message/HelpOp.java
+++ src/me/devtec/scr/commands/message/HelpOp.java
@@ -1,5 +1,21 @@
 package me.devtec.scr.commands.message;
 
-public class HelpOp {
+import java.util.List;
+
+import me.devtec.scr.commands.ScrCommand;
+
+public class HelpOp implements ScrCommand {
+
+	@Override
+	public void init(List<String> cmds) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String configSection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
