e10f84409fbec52791a81a9d6cd6243b20251f28
StraikerinaCZ <Straiker123@email.cz>
2022-08-05 20:33:41 +0200

    .

--- src/me/devtec/scr/commands/fun/God.java
+++ src/me/devtec/scr/commands/fun/God.java
@@ -19,152 +19,125 @@ public class God implements ScrCommand {
 
 	public static List<UUID> antiDamage;
 	boolean legacy = Ref.isOlderThan(10);
-	
+
 	@Override
 	public void init(List<String> cmds) {
-		if(legacy) { //We have to use listener
+		if (legacy) { // We have to use listener
 			antiDamage = new ArrayList<>();
-	        Loader.plugin.getLogger().info("[God] Using AntiDamage listener");
+			Loader.plugin.getLogger().info("[God] Using AntiDamage listener");
 			Loader.registerListener(new AntiDamage());
 		}
-		
+
 		CommandStructure.create(CommandSender.class, PERMS_CHECKER, (s, structure, args) -> { // cmd
-			if(s instanceof Player) {
-				Player p = (Player)s;
-				if(legacy) {
+			if (s instanceof Player) {
+				Player p = (Player) s;
+				if (legacy)
 					applyLegacy(p, isInvulnerable(p));
-				}else {
+				else
 					applyModern(p, isInvulnerable(p));
-				}
-				
+
 				String status = isInvulnerable(p) ? "enabled" : "disabled";
-				msgSec(s, ""+status);
-			}else {
+				msgSec(s, "" + status);
+			} else
 				help(s, "admin_usage");
-			}
-		}).cooldownDetection((s, structure, args) -> inCooldown(s))
-		.permission(permission("cmd"))
-		.fallback((s, structure, args) -> {
+		}).cooldownDetection((s, structure, args) -> inCooldown(s)).permission(permission("cmd")).fallback((s, structure, args) -> {
 			offlinePlayer(s, args[0]);
-			}).permission(permission("cmd"))
-			.argument("-s", (s, structure, args) -> { // cmd -s
-				if(s instanceof Player) {
-					Player p = (Player)s;
-					if(legacy) {
-						applyLegacy(p, isInvulnerable(p));
-					}else {
-						applyModern(p, isInvulnerable(p));
-					}
-				}else {
-					help(s, "admin_usage");
-				}
-			})
-			.parent() // cmd 
-			.selector(Selector.BOOLEAN, (s, structure, args) -> { // cmd [boolean]
-				if(s instanceof Player) {
-					Player p = (Player)s;
-					if(legacy) {
-						applyLegacy(p, !Boolean.parseBoolean(args[0]));
-					}else {
-						applyModern(p, !Boolean.parseBoolean(args[0]));
-					}
-					
-					String status = Boolean.parseBoolean(args[0]) ? "enabled" : "disabled";
-					msgSec(s, ""+status);
-				}else {
-					help(s, "admin_usage");
-				}
-				})
-				.argument("-s", (s, structure, args) -> { // cmd [boolean] -s
-					if(s instanceof Player) {
-						Player p = (Player)s;
-						if(legacy) {
-							applyLegacy(p, !Boolean.parseBoolean(args[0]));
-						}else {
-							applyModern(p, !Boolean.parseBoolean(args[0]));
-						}
-					}else {
-						help(s, "admin_usage");
-					}
-				})
-				.parent() // cmd [boolean]
-			.parent() // cmd
-			.selector(Selector.ENTITY_SELECTOR, (s, structure, args) -> { // cmd [entity_selector]
-				for(Player p : playerSelectors(s, args[0])) {
-					if(legacy) {
-						applyLegacy(p, isInvulnerable(p));
-					}else {
-						applyModern(p, isInvulnerable(p));
-					}
-					
-					String status = isInvulnerable(p) ? "enabled" : "disabled";
-					msgSec(s, "other."+status+".sender", Placeholders.c().addPlayer("target", p));
-					msgSec(p, "other."+status+".target", Placeholders.c().addPlayer("player", s));
-				}
-			}).permission(permission("other"))
-			.fallback((s, structure, args) -> {
+		}).permission(permission("cmd")).argument("-s", (s, structure, args) -> { // cmd -s
+			if (s instanceof Player) {
+				Player p = (Player) s;
+				if (legacy)
+					applyLegacy(p, isInvulnerable(p));
+				else
+					applyModern(p, isInvulnerable(p));
+			} else
 				help(s, "admin_usage");
-				})
-				.argument("-s", (s, structure, args) -> { // cmd [entity_selector] -s
-					for(Player p : playerSelectors(s, args[0])) {
-						if(legacy) {
+		}).parent() // cmd
+				.selector(Selector.BOOLEAN, (s, structure, args) -> { // cmd [boolean]
+					if (s instanceof Player) {
+						Player p = (Player) s;
+						if (legacy)
+							applyLegacy(p, !Boolean.parseBoolean(args[0]));
+						else
+							applyModern(p, !Boolean.parseBoolean(args[0]));
+
+						String status = Boolean.parseBoolean(args[0]) ? "enabled" : "disabled";
+						msgSec(s, "" + status);
+					} else
+						help(s, "admin_usage");
+				}).argument("-s", (s, structure, args) -> { // cmd [boolean] -s
+					if (s instanceof Player) {
+						Player p = (Player) s;
+						if (legacy)
+							applyLegacy(p, !Boolean.parseBoolean(args[0]));
+						else
+							applyModern(p, !Boolean.parseBoolean(args[0]));
+					} else
+						help(s, "admin_usage");
+				}).parent() // cmd [boolean]
+				.parent() // cmd
+				.selector(Selector.ENTITY_SELECTOR, (s, structure, args) -> { // cmd [entity_selector]
+					for (Player p : playerSelectors(s, args[0])) {
+						if (legacy)
 							applyLegacy(p, isInvulnerable(p));
-						}else {
+						else
 							applyModern(p, isInvulnerable(p));
-						}
+
+						String status = isInvulnerable(p) ? "enabled" : "disabled";
+						msgSec(s, "other." + status + ".sender", Placeholders.c().addPlayer("target", p));
+						msgSec(p, "other." + status + ".target", Placeholders.c().addPlayer("player", s));
 					}
-				})
-				.parent() // cmd [entity_selector]
+				}).permission(permission("other")).fallback((s, structure, args) -> {
+					help(s, "admin_usage");
+				}).argument("-s", (s, structure, args) -> { // cmd [entity_selector] -s
+					for (Player p : playerSelectors(s, args[0]))
+						if (legacy)
+							applyLegacy(p, isInvulnerable(p));
+						else
+							applyModern(p, isInvulnerable(p));
+				}).parent() // cmd [entity_selector]
 				.selector(Selector.BOOLEAN, (s, structure, args) -> { // cmd [entity_selector] [boolean]
-					for(Player p : playerSelectors(s, args[0])) {
-						if(legacy) {
+					for (Player p : playerSelectors(s, args[0])) {
+						if (legacy)
 							applyLegacy(p, !Boolean.parseBoolean(args[1]));
-						}else {
+						else
 							applyModern(p, !Boolean.parseBoolean(args[1]));
-						}
-						
+
 						String status = Boolean.parseBoolean(args[1]) ? "enabled" : "disabled";
-						msgSec(s, "other."+status+".sender", Placeholders.c().addPlayer("target", p));
-						msgSec(p, "other."+status+".target", Placeholders.c().addPlayer("player", s));
+						msgSec(s, "other." + status + ".sender", Placeholders.c().addPlayer("target", p));
+						msgSec(p, "other." + status + ".target", Placeholders.c().addPlayer("player", s));
 					}
-					})
-					.argument("-s", (s, structure, args) -> { // cmd [entity_selector] [boolean] -s
-						for(Player p : playerSelectors(s, args[0])) {
-							if(legacy) {
-								applyLegacy(p, !Boolean.parseBoolean(args[1]));
-							}else {
-								applyModern(p, !Boolean.parseBoolean(args[1]));
-							}
-						}
-					}).build().register(cmds.remove(0), cmds.toArray(new String[0]));
+				}).argument("-s", (s, structure, args) -> { // cmd [entity_selector] [boolean] -s
+					for (Player p : playerSelectors(s, args[0]))
+						if (legacy)
+							applyLegacy(p, !Boolean.parseBoolean(args[1]));
+						else
+							applyModern(p, !Boolean.parseBoolean(args[1]));
+				}).build().register(cmds.remove(0), cmds.toArray(new String[0]));
 	}
-	
+
 	public boolean isInvulnerable(Player p) {
-		if(!legacy) {
+		if (!legacy)
 			return p.isInvulnerable();
-		}
 		return antiDamage.contains(p.getUniqueId());
 	}
-	
+
 	public void applyLegacy(Player p, boolean status) {
-		if(status) {
+		if (status)
 			antiDamage.remove(p.getUniqueId());
-		}else {
+		else
 			antiDamage.add(p.getUniqueId());
-		}
 	}
-	
+
 	public void applyModern(Player p, boolean status) {
-		if(status) {
+		if (status)
 			p.setInvulnerable(false);
-		}else {
+		else
 			p.setInvulnerable(true);
-		}
 	}
 
 	@Override
 	public String configSection() {
 		return "god";
 	}
-	
+
 }
--- src/me/devtec/scr/commands/message/privatemessage/Message.java
+++ src/me/devtec/scr/commands/message/privatemessage/Message.java
@@ -17,48 +17,39 @@ public class Message implements ScrCommand {
 
 	@Override
 	public void init(List<String> cmds) {
-		
+
 		Loader.plugin.getLogger().info("[PrivateMessage] Using message listener");
 		Loader.registerListener(new PrivateMessageListener());
-		
+
 		CommandStructure.create(CommandSender.class, PERMS_CHECKER, (s, structure, args) -> { // cmd
-			if(MessageManager.chatLock.containsKey(s.getName()))
+			if (MessageManager.chatLock.containsKey(s.getName()))
 				MessageManager.chatLock(s, null);
 			else
 				help(s, "usage");
-		}).cooldownDetection((s, structure, args) -> inCooldown(s))
-		.permission(permission("cmd"))
-		.fallback((s, structure, args) -> { // /pm [player]
+		}).cooldownDetection((s, structure, args) -> inCooldown(s)).permission(permission("cmd")).fallback((s, structure, args) -> { // /pm [player]
 			offlinePlayer(s, args[0]);
-		})
-			.selector(Selector.ENTITY_SELECTOR, (s, structure, args) -> { // /pm [player]
-				for(Player p : playerSelectors(s, args[0])) {
-					MessageManager.message(s, p, null);
-				}
-			}).priority(1)
-				.argument(null, (s, structure, args) -> { // /pm [player] [message]
-					for(Player p : playerSelectors(s, args[0])) {
-						MessageManager.message(s, p, StringUtils.buildString(1, args));
-					}
-					
-				})
-				.parent() // pm [player]
-			.parent()// cmd
-			.argument("CONSOLE", (s, structure, args) -> { // /pm [console]
-				MessageManager.message(s, Bukkit.getConsoleSender(), null);
-			}).priority(2)
-				.argument(null, (s, structure, args) -> { // /pm [console] [message]
+		}).selector(Selector.ENTITY_SELECTOR, (s, structure, args) -> { // /pm [player]
+			for (Player p : playerSelectors(s, args[0]))
+				MessageManager.message(s, p, null);
+		}).priority(1).argument(null, (s, structure, args) -> { // /pm [player] [message]
+			for (Player p : playerSelectors(s, args[0]))
+				MessageManager.message(s, p, StringUtils.buildString(1, args));
+
+		}).parent() // pm [player]
+				.parent()// cmd
+				.argument("CONSOLE", (s, structure, args) -> { // /pm [console]
+					MessageManager.message(s, Bukkit.getConsoleSender(), null);
+				}).priority(2).argument(null, (s, structure, args) -> { // /pm [console] [message]
 					MessageManager.message(s, Bukkit.getConsoleSender(), StringUtils.buildString(1, args));
-					
+
+				}).parent() // pm [console]
+				.parent() // cmd
+				.argument(null, (s, structure, args) -> { // /pm [message] //if target is locked
+					if (MessageManager.chatLock.containsKey(s.getName()))
+						MessageManager.message(s, null, StringUtils.buildString(args));
 				})
-				.parent() // pm [console]
-			.parent() // cmd
-			.argument(null, (s, structure, args) -> { // /pm [message] //if target is locked
-				if(MessageManager.chatLock.containsKey(s.getName()))
-					MessageManager.message(s, null, StringUtils.buildString(args));
-			})
-			
-		.build().register(cmds.remove(0), cmds.toArray(new String[0]));
+
+				.build().register(cmds.remove(0), cmds.toArray(new String[0]));
 	}
 
 	@Override
