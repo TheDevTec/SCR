82b400f9d588aad0e02f5b5e72ca34138aa365c2
Houska02 <ADAMBABOVAK@seznam.cz>
2022-08-12 18:32:42 +0200

    ChatLock command

---
+++ src/me/devtec/scr/commands/server_managment/ChatLock.java
@@ -0,0 +1,32 @@
+package me.devtec.scr.commands.server_managment;
+
+import java.util.List;
+
+import org.bukkit.command.CommandSender;
+
+import me.devtec.scr.commands.ScrCommand;
+import me.devtec.scr.utils.ChatUtils;
+import me.devtec.shared.commands.structures.CommandStructure;
+
+public class ChatLock implements ScrCommand {
+
+	@Override
+	public void init(List<String> cmds) {
+
+		CommandStructure.create(CommandSender.class, PERMS_CHECKER, (s, structure, args) -> {
+			if(ChatUtils.isChatLocked())
+				msgSec(s, "enabled");
+			else
+				msgSec(s, "disabled");
+			ChatUtils.lockChat();
+		}).cooldownDetection((s, structure, args) -> inCooldown(s))
+		.permission(permission("cmd")) // perm
+		.build().register(cmds.remove(0), cmds.toArray(new String[0]));
+	}
+
+	@Override
+	public String configSection() {
+		return "chatlock";
+	}
+
+}
