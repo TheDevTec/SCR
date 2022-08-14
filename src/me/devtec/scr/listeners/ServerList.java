8d97e397e44810cbecec62853257356b4f404055
StraikerinaCZ <Straiker123@email.cz>
2022-08-14 19:48:57 +0200

    Fixed Java 1.8 errors

--- src/me/devtec/scr/listeners/ServerList.java
+++ src/me/devtec/scr/listeners/ServerList.java
@@ -17,81 +17,75 @@ import me.devtec.shared.events.EventListener;
 import me.devtec.theapi.bukkit.events.ServerListPingEvent;
 import me.devtec.theapi.bukkit.events.ServerListPingEvent.PlayerProfile;
 
-
 public class ServerList implements EventListener {
-	
+
 	@Override
 	public void listen(Event event) {
 		Loader.plugin.getLogger().info("OMG OMG");
-		if(event instanceof ServerListPingEvent e) {
+		if (event instanceof ServerListPingEvent) {
+			ServerListPingEvent e = (ServerListPingEvent) event;
 			Loader.plugin.getLogger().info("1");
-			//ServerListPingEvent e = (ServerListPingEvent) event;
+			// ServerListPingEvent e = (ServerListPingEvent) event;
 
-			if(!Loader.config.getBoolean("serverlist.enabled"))
+			if (!Loader.config.getBoolean("serverlist.enabled"))
 				return;
-			//TODO - if maintenance (mohlo by být přídáno do MOTD.getUsed() :D
+			// TODO - if maintenance (mohlo by být přídáno do MOTD.getUsed() :D
 
 			Loader.plugin.getLogger().info("2");
 			String motd_name = MOTD.getUsed();
-			
-			
-			//MOTD
-			if(MOTD.getMotd(motd_name)!=null)
-				e.setMotd(PlaceholderAPISupport.replace(MOTD.getMotd(motd_name), null, false)  );
+
+			// MOTD
+			if (MOTD.getMotd(motd_name) != null)
+				e.setMotd(PlaceholderAPISupport.replace(MOTD.getMotd(motd_name), null, false));
 			Loader.plugin.getLogger().info("3");
-			
-			//ONLINE
-			if(Loader.config.exists("serverlist."+motd_name+".players.online"))
+
+			// ONLINE
+			if (Loader.config.exists("serverlist." + motd_name + ".players.online"))
 				e.setOnlinePlayers(MOTD.getPlayers(motd_name, PlayersCountType.ONLINE));
 			Loader.plugin.getLogger().info("4");
 
-			//MAXIMAL
-			if(Loader.config.exists("serverlist."+motd_name+".players.max"))
+			// MAXIMAL
+			if (Loader.config.exists("serverlist." + motd_name + ".players.max"))
 				e.setMaxPlayers(MOTD.getPlayers(motd_name, PlayersCountType.MAX));
 			Loader.plugin.getLogger().info("5");
-			
-			//PLAYERS
-			if(Loader.config.exists("serverlist."+motd_name+".players.list") && 
-					!Loader.config.getStringList("serverlist."+motd_name+".players.list").isEmpty()) {
-				//LIST
+
+			// PLAYERS
+			if (Loader.config.exists("serverlist." + motd_name + ".players.list") && !Loader.config.getStringList("serverlist." + motd_name + ".players.list").isEmpty()) {
+				// LIST
 				List<String> list = MOTD.getList(motd_name);
 				List<PlayerProfile> profiles = new ArrayList<>(list.size());
-				for(String s : list)
+				for (String s : list)
 					profiles.add(new PlayerProfile(s));
-				e.setPlayersText(profiles);			
-			}else {
-				//CUSTOM PLAYER NAMES
-				boolean hide = Loader.config.getBoolean("serverlist."+motd_name+".players.hide-vanished");
+				e.setPlayersText(profiles);
+			} else {
+				// CUSTOM PLAYER NAMES
+				boolean hide = Loader.config.getBoolean("serverlist." + motd_name + ".players.hide-vanished");
 				Iterator<PlayerProfile> p = e.getPlayersText().iterator();
 				while (p.hasNext()) {
 					PlayerProfile profile = p.next();
 					Player player = Bukkit.getPlayer(profile.getUUID());
-					if(player!=null && hide && API.isVanished(player))
+					if (player != null && hide && API.isVanished(player))
 						p.remove();
 					else
-						profile.setName( PlaceholderAPISupport.replace(
-								Loader.config.getString("serverlist."+motd_name+".players.playername-format")
-								, player, true) 
-								);
+						profile.setName(PlaceholderAPISupport.replace(Loader.config.getString("serverlist." + motd_name + ".players.playername-format"), player, true));
 				}
 			}
 			Loader.plugin.getLogger().info("6");
-			
-			//ICON
-			if(MOTD.getIcon(motd_name)!=null)
+
+			// ICON
+			if (MOTD.getIcon(motd_name) != null)
 				e.setFalvicon(MOTD.getIcon(motd_name));
 			Loader.plugin.getLogger().info("7");
-			
-			//SERVER VERSION
-			if(MOTD.getVersion(motd_name)!=null)
+
+			// SERVER VERSION
+			if (MOTD.getVersion(motd_name) != null)
 				e.setVersion(MOTD.getVersion(motd_name));
 
-			//PROTOCOL
-			if(MOTD.getProtocol(motd_name)!=-1)
+			// PROTOCOL
+			if (MOTD.getProtocol(motd_name) != -1)
 				e.setProtocol(MOTD.getProtocol(motd_name));
 			Loader.plugin.getLogger().info("8");
-			
+
 		}
-		return;
 	}
 }
--- src/me/devtec/scr/utils/ChatUtils.java
+++ src/me/devtec/scr/utils/ChatUtils.java
@@ -7,65 +7,59 @@ import me.devtec.scr.functions.Tablist;
 import me.devtec.shared.dataholder.Config;
 
 public class ChatUtils {
-	
-	//LOCK
-	public static void lockChat() { //TODO
-		if(Loader.data.getBoolean("chatlock", false))
+
+	// LOCK
+	public static void lockChat() { // TODO
+		if (Loader.data.getBoolean("chatlock", false))
 			Loader.data.set("chatlock", false);
 		else
 			Loader.data.set("chatlock", true);
 		Loader.data.save();
-		
+
 	}
-	public static boolean isChatLocked() { //TODO
+
+	public static boolean isChatLocked() { // TODO
 		return Loader.data.getBoolean("chatlock", false);
 	}
-	
-	//CHATFORMAT
-	public class ChatFormat {
-		
+
+	// CHATFORMAT
+	public static class ChatFormat {
+
 		private static Config c = Loader.chat;
-		
+
 		public static boolean isEnabled() {
 			return Loader.chat.getBoolean("format.enabled");
 		}
+
 		/*
-		 * format:
-		 *   global:
-		 *   group:
-		 *     <group>
-		 *   user:
-		 *     <user>
-		 *   world:
-		 *     <world>:
-		 *       repeat...
+		 * format: global: group: <group> user: <user> world: <world>: repeat...
 		 */
 		public static String getPath(Player p) {
 			String world = p.getLocation().getWorld().getName();
-			
-			if(c.exists("format.world." + world) ) { //World formats:
-				if(c.exists("format.world." + world +".user."+p.getName()) )
-					return "format.world." + world +".user."+p.getName();
-				if(c.exists("format.world." + world +".group." + Tablist.getVaultGroup(p)))
-					return "format.world." + world +".group." + Tablist.getVaultGroup(p);
-				if(c.exists("format.world." + world +".global"))
-					return "format.world." + world +".global";
+
+			if (c.exists("format.world." + world)) { // World formats:
+				if (c.exists("format.world." + world + ".user." + p.getName()))
+					return "format.world." + world + ".user." + p.getName();
+				if (c.exists("format.world." + world + ".group." + Tablist.getVaultGroup(p)))
+					return "format.world." + world + ".group." + Tablist.getVaultGroup(p);
+				if (c.exists("format.world." + world + ".global"))
+					return "format.world." + world + ".global";
 			}
-			
-			if(c.exists( "format.user." + p.getName() ) )
+
+			if (c.exists("format.user." + p.getName()))
 				return "format.user." + p.getName();
-			if(c.exists( "format.group." + Tablist.getVaultGroup(p) ) )
+			if (c.exists("format.group." + Tablist.getVaultGroup(p)))
 				return "format.group." + Tablist.getVaultGroup(p);
-			if(c.exists( "format.global" ) )
-				return "format.global";
-			
+			if (c.exists("format.global")) {
+			}
+
 			return "format.global";
 		}
 	}
 
-	//NOTIFICATION
-	public class Notification {
-		
+	// NOTIFICATION
+	public static class Notification {
+
 		public static boolean isEnabled() {
 			return Loader.chat.getBoolean("chatNotification.enabled");
 		}
