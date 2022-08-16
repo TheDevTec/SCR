9c473f6239185d82cdf27e301afba86bf3b3b08b
Houska02 <ADAMBABOVAK@seznam.cz>
2022-08-16 16:00:41 +0200

    Maintenance

--- src/me/devtec/scr/listeners/additional/PlayerJoin.java
+++ src/me/devtec/scr/listeners/additional/PlayerJoin.java
@@ -4,15 +4,20 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerLoginEvent.Result;
 
 import me.devtec.scr.Loader;
 import me.devtec.scr.MessageUtils;
 import me.devtec.scr.MessageUtils.Placeholders;
 import me.devtec.scr.api.API;
+import me.devtec.scr.utils.PlaceholderAPISupport;
 import me.devtec.shared.dataholder.Config;
 import me.devtec.shared.scheduler.Tasker;
+import me.devtec.shared.utility.StringUtils;
 import me.devtec.theapi.bukkit.BukkitLoader;
 
 public class PlayerJoin implements Listener {
@@ -22,14 +27,25 @@ public class PlayerJoin implements Listener {
 		this.joinconfig = config;
 	}
 
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void login(PlayerLoginEvent e) {
+		Player p = e.getPlayer();
+		if(Loader.data.getBoolean("maintenance")) {
+			String kick = "";
+			for(String s : Loader.translations.getStringList("maintenance.kickMessages"))
+				if(s.endsWith("\n"))
+					kick = kick+s;
+				else
+					kick = kick+s+"\n";
+			e.disallow(Result.KICK_WHITELIST, 
+					StringUtils.colorize(PlaceholderAPISupport.replace(kick, p, true)));
+			return;
+		}
+	}
+	
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		event.setJoinMessage(null);
-		
-		if(Loader.data.getBoolean("maintenance")) {
-			
-		}
-		
 		if(joinconfig.getBoolean("enabled")) {
 			if (!event.getPlayer().hasPlayedBefore()) {
 				for (String command : joinconfig.getStringList("firstTime.commands"))
